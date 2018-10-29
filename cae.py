787559db831b1bbd7319eba9bc33ee468dd4b919
Parcollet Titouan <titouan.parcollet@alumni.univ-avignon.fr>
2018-10-29 17:43:32 +0100

    cae.py

--- cae.py
+++ cae.py
@@ -24,7 +24,7 @@ def rgb2gray(rgb):
     gray = np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
     return np.repeat(gray[:, :, np.newaxis], 3, axis=2)
 
-if len(str(sys.argv) > 1):
+if len(sys.argv > 1):
     model       = str(sys.argv[1])
 else:
     print("Please provide a model : QCAE or CAE")
